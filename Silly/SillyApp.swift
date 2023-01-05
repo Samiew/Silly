b7b2f8726bacc9b596d5e0c1a02a49de0b7d347a
Samiew <111298357+Samiew@users.noreply.github.com>
2023-01-05 08:50:33 -0700

    Initial commit

---
+++ Silly/SillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SillyApp.swift
+//  Silly
+//
+//  Created by Wunderli, Samie on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
